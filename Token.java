2794fe7398aadcdef9abcb851af120c1cbbf9ae6
Nathan Janzen <moddington@gmail.com>
2016-02-29 15:06:48 -0700

    Added Token and Tokenizer classes.

---
+++ Token.java
@@ -0,0 +1,81 @@
+/**
+ * Base class for tokens: terminal symbols used in parsing.
+ */
+abstract class Token<T> {
+	public enum TokenType {
+		LeftParenthesis,
+		RightParenthesis,
+		Identifier,
+		Integer,
+		Float,
+		String,
+		Character,
+	}
+
+	private final TokenType _type;
+	private final T _value;
+
+	/**
+	 * Constructor that sets the token's type and value.
+	 */
+	public Token(TokenType type, T value) {
+		_type = type;
+		_value = value;
+	}
+
+	/**
+	 * Returns the token's type.
+	 */
+	public TokenType getType() {
+		return _type;
+	}
+
+	/**
+	 * Returns the token's value.
+	 */
+	public T getValue() {
+		return _value;
+	}
+}
+
+class TokenLeftParenthesis extends Token<Void> {
+	public TokenLeftParenthesis() {
+		super(TokenType.LeftParenthesis, null);
+	}
+}
+
+class TokenRightParenthesis extends Token<Void> {
+	public TokenRightParenthesis() {
+		super(TokenType.RightParenthesis, null);
+	}
+}
+
+class TokenIdentifier extends Token<String> {
+	public TokenIdentifier(String value) {
+		super(TokenType.Identifier, value);
+	}
+}
+
+class TokenInteger extends Token<Integer> {
+	public TokenInteger(Integer value) {
+		super(TokenType.Integer, value);
+	}
+}
+
+class TokenFloat extends Token<Float> {
+	public TokenFloat(Float value) {
+		super(TokenType.Float, value);
+	}
+}
+
+class TokenString extends Token<String> {
+	public TokenString(String value) {
+		super(TokenType.String, value);
+	}
+}
+
+class TokenCharacter extends Token<Character> {
+	public TokenCharacter(Character value) {
+		super(TokenType.Character, value);
+	}
+}
---
+++ Tokenizer.java
@@ -0,0 +1,41 @@
+/**
+ * Turns text input into a series of Token objects.
+ */
+class Tokenizer {
+	private String _text;
+
+	/**
+	 * Default constructor.
+	 */
+	public Tokenizer() {
+		_text = "";
+	}
+
+	/**
+	 * Constructor that provides some initial input text.
+	 *
+	 * @param text initial input text
+	 */
+	public Tokenizer(String text) {
+		_text = text;
+	}
+
+	/**
+	 * Provides additional text to be read.
+	 *
+	 * @param text additional input text
+	 */
+	public void provide(String text) {
+		_text += text;
+	}
+
+	/**
+	 * Reads a token from the remaining input text.
+	 *
+	 * @return the Token object that was parsed from the text
+	 */
+	public Token readToken() {
+		// TODO
+		return null;
+	}
+}
