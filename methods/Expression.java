86da4ae2480a568cffecfbcabc49c6f678a756b5
alexander19lam <alexander.lam@ucalgary.ca>
2016-03-17 01:50:17 -0600

    Updated to work with ParseException

--- methods/Expression.java
+++ methods/Expression.java
@@ -1,5 +1,6 @@
 package methods;
 
+import java.text.ParseException;
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -8,7 +9,7 @@ abstract class Expression {
 	 * Evaluates the expression and returns its value.
 	 * Runtime type checking will be necessary to determine the value's type.
 	 */
-	public abstract Object evaluate(Class commands) throws Exception;
+	public abstract Object evaluate(Class commands) throws ParseException;
 }
 
 class Expression_FunctionCall extends Expression {
@@ -28,7 +29,7 @@ class Expression_FunctionCall extends Expression {
 		_right_paren = right_paren;
 	}
 
-	public Object evaluate(Class commands) throws Exception{
+	public Object evaluate(Class commands) throws ParseException{
 		Object[] parameters = new Object[_arguments.size()];
 		for (int i = 0; i < _arguments.size(); i++) {
 			parameters[i] = _arguments.get(i).evaluate(commands);
@@ -43,31 +44,39 @@ class Expression_FunctionCall extends Expression {
 		}
 
 		String name = _identifier.getValue();
-		for (Method method : commands.getDeclaredMethods()) {
-			if (!name.equals(method.getName())) continue;
-
-			Class[] parameter_types = method.getParameterTypes();
-			if (types.length != parameter_types.length) continue;
-
-			boolean types_matched = true;
-			for (int i = 0; i < types.length; ++i) {
-				// Normalize int and float
-				if (parameter_types[i] == int.class) parameter_types[i] = Integer.class;
-				if (parameter_types[i] == float.class) parameter_types[i] = Float.class;
-
-				if (types[i] != parameter_types[i]) {
-					types_matched = false;
-					break;
+		try{
+			for (Method method : commands.getDeclaredMethods()) {
+				if (!name.equals(method.getName())) continue;
+	
+				Class[] parameter_types = method.getParameterTypes();
+				if (types.length != parameter_types.length) continue;
+	
+				boolean types_matched = true;
+				for (int i = 0; i < types.length; ++i) {
+					// Normalize int and float
+					if (parameter_types[i] == int.class) parameter_types[i] = Integer.class;
+					if (parameter_types[i] == float.class) parameter_types[i] = Float.class;
+	
+					if (types[i] != parameter_types[i]) {
+						types_matched = false;
+						break;
+					}
 				}
+				if (!types_matched) continue;
+	
+				// If all checks succeeded, we have found the method
+				return method.invoke(null, parameters);
 			}
-			if (!types_matched) continue;
-
-			// If all checks succeeded, we have found the method
-			return method.invoke(null, parameters);
+	
+			// No method has passed all the checks, the method could not be found
+			throw new NoSuchMethodException(name);
+		}catch (Exception e){
+			String params = "";
+			for(Class c: types){
+				params += " " + c.toString();
+			}
+			throw new ParseException("Matching function for ("+ name + params +") not found", _identifier.getLocation().getColumn());		
 		}
-
-		// No method has passed all the checks, the method could not be found
-		throw new NoSuchMethodException(name);
 	}
 }
 
@@ -78,7 +87,7 @@ class Expression_Integer extends Expression {
 		_symbol = symbol;
 	}
 
-	public Integer evaluate(Class commands) throws Exception {
+	public Integer evaluate(Class commands) throws ParseException {
 		return _symbol.getValue();
 	}
 }
@@ -90,7 +99,7 @@ class Expression_Float extends Expression {
 		_symbol = symbol;
 	}
 
-	public Float evaluate(Class commands) throws Exception {
+	public Float evaluate(Class commands) throws ParseException {
 		return _symbol.getValue();
 	}
 }
@@ -102,7 +111,7 @@ class Expression_String extends Expression {
 		_symbol = symbol;
 	}
 
-	public String evaluate(Class commands)throws Exception {
+	public String evaluate(Class commands)throws ParseException {
 		return _symbol.getValue();
 	}
 }
