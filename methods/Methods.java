d1216f2fc5596c964e91d2d9c74a3c277fd9f87c
Nathan Janzen <moddington@gmail.com>
2016-03-17 12:26:52 -0600

    Cleaned up argument handling in main()

--- methods/Methods.java
+++ methods/Methods.java
@@ -14,7 +14,7 @@ public class Methods {
     //TODO: ERROR CHECKING
     // Fields
     static String className = "Commands";
-    static String jarName;
+    static String jarName = null;
     static boolean  verbose = false;
     static Class  command;
 
@@ -24,21 +24,7 @@ public class Methods {
             "?           : Print this helpful text.\n" +
             "<expression>: Evaluate the expression.\n" +
             "Expressions can be integers, floats, strings (surrounded in double quotes) or function\n" +
-            "calls of the form '(identifier {expression}*)'.";
-    static final String HELPTEXT = "Synopsis:\n" +
-            "  methods\n" +
-            "  methods { -h | -? | --help }+\n" +
-            "  methods {-v --verbose}* <jar-file> [<class-name>]\n" +
-            "Arguments:\n" +
-            "  <jar-file>:   The .jar file that contains the class to load (see next line).\n" +
-            "  <class-name>: The fully qualified class name containing public static command methods to call. [Default=\"Commands\"]\n" +
-            "Qualifiers:\n" +
-            "  -v --verbose: Print out detailed errors, warning, and tracking.\n" +
-            "  -h -? --help: Print out a detailed help message.\n" +
-            "Single-char qualifiers may be grouped; long qualifiers may be truncated to unique prefixes and are not case sensitive.\n" +
-            "\n" +
-            "This program interprets commands of the format '(<method> {arg}*)' on the command line, finds corresponding\n" +
-            "methods in <class-name>, and executes them, printing the result to sysout.";
+            "calls of the form '(identifier {expression}*)'.\n";
     static final String SYNOPSIS = "Synopsis:\n" +
             "  methods\n" +
             "  methods { -h | -? | --help }+\n" +
@@ -50,14 +36,17 @@ public class Methods {
             "  -v --verbose: Print out detailed errors, warning, and tracking.\n" +
             "  -h -? --help: Print out a detailed help message.\n" +
             "Single-char qualifiers may be grouped; long qualifiers may be truncated to unique prefixes and are not case sensitive.\n";
+    static final String HELPTEXT = "This program interprets commands of the format '(<method> {arg}*)' on the command line, finds corresponding\n" +
+            "methods in <class-name>, and executes them, printing the result to sysout.\n";
 
 
 
     public static void main(String[] argv) {
-        boolean help = false;
-        boolean noQualifier = true;
-        int i;
 
+
+		// Read short and long flags
+        boolean help = false;
+        int i;
         for (i = 0; i < argv.length; i++) {
             String argument = argv[i];
             //Check long flags
@@ -67,7 +56,6 @@ public class Methods {
                     help = true;
                 } else if (argument.toLowerCase().equals("verbose")) {
                     verbose = true;
-
                 } else {
                     System.exit(FatalErrors.unrecognizedQualifier(argument));
                 }
@@ -75,10 +63,8 @@ public class Methods {
                 for (char c : argument.substring(1).toCharArray()) {
                     if (c == '?' | c == 'h') {
                         help = true;
-                        noQualifier = false;
                     } else if (c == 'v') {
                         verbose = true;
-                        noQualifier = false;
                     } else {
                         System.exit(FatalErrors.unrecognizedQualifier(c, argument));
                     }
@@ -87,6 +73,7 @@ public class Methods {
                 break;
             }
         }
+		// Read positional arguments
         for (int p = 0; i < argv.length; i++, p++){
             if (argv[i].startsWith("-")){
                 System.exit(FatalErrors.invalidOrder());
@@ -97,14 +84,22 @@ public class Methods {
                 className = argv[i];
             }else {
                 System.exit(FatalErrors.invalidCommandLineArguments());
-
             }
         }
 
 
-        //Print necessary text
-        if (noQualifier) System.out.println(SYNOPSIS);
-        if (help) System.out.println(HELPTEXT);
+		// Help mode; print synopsis, help text, and exit
+		if (help) {
+			System.out.print(SYNOPSIS);
+			System.out.print("\n");
+			System.out.print(HELPTEXT);
+			System.exit(0);
+		}
+		// Invalid arguments; print synopsis and exit
+		if (jarName == null) {
+			System.out.print(SYNOPSIS);
+			System.exit(0);
+		}
 
         //Getting input
         String input;
@@ -120,7 +115,7 @@ public class Methods {
         }
 
 
-        System.out.println(INITIALIZATIONTEXT);
+        System.out.print(INITIALIZATIONTEXT);
 		while (true){
             System.out.print("> ");
 			if (s.hasNextLine()) {
