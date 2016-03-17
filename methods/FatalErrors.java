273170e0edf0718dd5d38f9a67e05ef0b830834e
Nathan Janzen <moddington@gmail.com>
2016-03-17 12:40:02 -0600

    Added synopsis printing for fatal errors.

--- methods/FatalErrors.java
+++ methods/FatalErrors.java
@@ -3,31 +3,39 @@ package methods;
 public class FatalErrors {
     public static int unrecognizedQualifier(String longQualifier){
         System.err.println("Unrecognized qualifier: --"+ longQualifier+ ".");
+		System.err.print(Methods.SYNOPSIS);
         return -1;
     }
+
     public static int unrecognizedQualifier(char letter,  String lettersInQualifier){
         System.err.println("Unrecognized qualifier '"+ letter +"' in '"+ lettersInQualifier +"'.");
+		System.err.print(Methods.SYNOPSIS);
         return -1;
     }
 
     public static int invalidCommandLineArguments(){
         System.err.println("This program takes at most two command line arguments.");
+		System.err.print(Methods.SYNOPSIS);
         return -2;
     }
+
     public static int invalidOrder(){
         System.err.println("This program requires a jar file as the first command line argument (after any qualifiers).");
+		System.err.print(Methods.SYNOPSIS);
         return -3;
     }
+
     public static int invalidHelp(){
         System.err.println("Qualifier --help (-h, -?) should not appear with any command-line arguments.");
+		System.err.print(Methods.SYNOPSIS);
         return -4;
-
     }
+
     public static int jarNotFound(String jarName){
         System.err.println("Could not load jar file: " + jarName);
         return -5;
-
     }
+
     public static int classNotFound(String className){
         System.err.println("Could not find class: " + className);
         return -6;
